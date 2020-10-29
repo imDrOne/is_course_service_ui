2a29fe5fd0745adf54b9e5eefc5845e1af600590
atiholoz <andre.tikholoz@s-telematics.ru>
2020-10-29 18:57:37 +0300

    install quasar; add base AuthLayout, MainLayout; add base MainView;

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+
+// Layouts
+import MainLayout from "@/layouts/MainLayout/index";
+import AuthLayout from "@/layouts/AuthLayout/index";
+
+//Views
+import MainView from "@/views/MainView/index";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    redirect: "/main",
+    component: MainLayout,
+    name: "Dashboard",
+    children: [
+      {
+        path: "main",
+        component: MainView,
+        name: "Main"
+      }
+    ]
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/",
+    redirect: "/auth/login",
+    component: AuthLayout,
+    name: "Auth"
   }
 ];
 
